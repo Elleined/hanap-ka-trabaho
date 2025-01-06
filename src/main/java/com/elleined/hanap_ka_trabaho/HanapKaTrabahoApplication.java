25a7ad3183b2eee0bbcb681d63e13d1b70eb4d3e
elleined <demadegu@gmail.com>
2025-01-06 09:04:48 +0800

    Initial commit + added applicatrion properties and added dependencies in pom.xml

---
+++ src/main/java/com/elleined/hanap_ka_trabaho/HanapKaTrabahoApplication.java
@@ -0,0 +1,13 @@
+package com.elleined.hanap_ka_trabaho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanapKaTrabahoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanapKaTrabahoApplication.class, args);
+	}
+
+}
